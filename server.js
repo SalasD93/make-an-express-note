499475c8800d84565bbb0e18e121cca9321e5c69
Diana Salas <salasd10418@gmail.com>
2021-08-28 01:48:05 -0400

    added base code and routes

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoute = require('./Routes/apiRoute/index');
+const htmlRoute = require('./Routes/htmlRoute/index');
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "Develop/public")));
+
+app.use("/api", apiRoute);
+
+app.use("/", htmlRoute);
+
+app.listen(PORT, () => {
+    console.log(`App listening on port: ${PORT}`);
+});
